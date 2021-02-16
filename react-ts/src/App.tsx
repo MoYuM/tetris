daba7389de46a11d7d4cc2b89e681cca5d48194b
MoYuM <MoYuM@github.com>
2021-02-16 23:02:53 +0800

    feat: block will stop when reach the bottom

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -28,6 +28,8 @@ const App = () => {
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     ]
   )
+  const [currentBlock, setCurrentBlock] = React.useState<number[]>([])
+  const [time, setTime] = React.useState<number>()
 
   const render = () => {
     return (
@@ -36,11 +38,11 @@ const App = () => {
           map.map((item: number, index: number) => {
             switch (item) {
               case 0:
-                return <div className="block" key={index} />;
+                return <div className="block" key={index} >{index}</div>;
               case 1:
-                return <div className="block-L" key={index} />
+                return <div className="block-L" key={index} >{index}</div>
               default:
-                return <div className="block" key={index} />;
+                return <div className="block" key={index} >{index}</div>;
             }
           })
         }
@@ -50,24 +52,42 @@ const App = () => {
 
   const create = () => {
     let initIndex = 4;
-    const dropdown = setInterval(() => {
+    const tim = window.setInterval(() => {
       create_L(initIndex);
       initIndex += 10
-    }, 1000);
+    }, 500);
+    setTime(tim);
   }
 
   const create_L = (initIndex: number) => {
     const indexList = [initIndex, initIndex + 10, initIndex + 20, initIndex + 21]
+    setCurrentBlock(indexList);
     const temp = JSON.parse(JSON.stringify(map));
     indexList.forEach(i => temp[i] = 1);
     setMap(temp);
   }
+
+  React.useEffect(() => {
+    let isEnd = false;
+    currentBlock
+      .filter(item => !currentBlock.includes(item + 10))
+      .forEach(item => {
+        if (map[item + 10] !== 0) {
+          isEnd = true;
+        }
+      })
+
+    if (isEnd) {
+      clearInterval(time);
+    }
+  }, [currentBlock])
 
   return (
     <div className="App">
       <Wrapper>
         {render()}
         <button onClick={create}>create_L</button>
+        <button onClick={() => {clearInterval(time)}}>stop</button>
       </Wrapper>
     </div>
   );
