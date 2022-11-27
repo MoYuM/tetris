99ada7dcc2c34e35fecd486162e24c5a5706adaf
MoYuM <hanbaochang12138@gmail.com>
2022-11-27 16:51:15 +0800

    chore: 格式化代码

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,142 +2,142 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-  public Board board { get; private set; }
-  public TetrominoData data { get; private set; }
-  public Vector3Int[] cells { get; private set; }
-  public Vector3Int position { get; private set; }
-  private int rotationIndex = 0;
+    public Board board { get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+    private int rotationIndex = 0;
 
-  public void Initialize(Board board, Vector3Int position, TetrominoData data)
-  {
-    this.board = board;
-    this.position = position;
-    this.data = data;
-
-    if (cells == null)
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-      cells = new Vector3Int[data.cells.Length];
+        this.board = board;
+        this.position = position;
+        this.data = data;
+
+        if (cells == null)
+        {
+            cells = new Vector3Int[data.cells.Length];
+        }
+
+        // grip the data from TetrominoData into Piece
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i] = (Vector3Int)data.cells[i];
+        }
     }
 
-    // grip the data from TetrominoData into Piece
-    for (int i = 0; i < cells.Length; i++)
+    private void Update()
     {
-      cells[i] = (Vector3Int)data.cells[i];
-    }
-  }
 
-  private void Update()
-  {
+        board.Clear(this);
 
-    board.Clear(this);
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(-1);
+        }
 
-    if (Input.GetKeyDown(KeyCode.Q))
-    {
-      Rotate(-1);
-    }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(1);
+        }
 
-    if (Input.GetKeyDown(KeyCode.E))
-    {
-      Rotate(1);
-    }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
 
-    if (Input.GetKeyDown(KeyCode.A))
-    {
-      Move(Vector2Int.left);
-    }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
 
-    if (Input.GetKeyDown(KeyCode.D))
-    {
-      Move(Vector2Int.right);
-    }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
 
-    if (Input.GetKeyDown(KeyCode.S))
-    {
-      Move(Vector2Int.down);
-    }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hardDrop();
+        }
 
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      hardDrop();
+        board.Set(this);
     }
 
-    board.Set(this);
-  }
-
-  public void Rotate(int direction)
-  {
-    rotationIndex += direction;
-    rotationIndex = Clamp(0, cells.Length, rotationIndex);
-
-    for (int i = 0; i < cells.Length; i++)
+    public void Rotate(int direction)
     {
-      Vector3 cell = cells[i];
+        rotationIndex += direction;
+        rotationIndex = Clamp(0, cells.Length, rotationIndex);
 
-      switch (data.tetromino)
-      {
-        case Tetromino.I:
-        case Tetromino.O:
-          cell.x += 0.5f;
-          cell.y += 0.5f;
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3 cell = cells[i];
 
-          cells[i] = Rotate90(direction, cell);
+            switch (data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x += 0.5f;
+                    cell.y += 0.5f;
 
-          break;
+                    cells[i] = Rotate90(direction, cell);
 
-        default:
+                    break;
 
-          cells[i] = Rotate90(direction, cell);
-          break;
-      }
-    }
-  }
+                default:
 
-public Vector3Int Rotate90(int direction, Vector3 position) 
-{ 
-  float x = -1 * direction * position.y;
-  float y = direction * position.x;
-
-  return new Vector3Int((int)x, (int)y, 0);
-}
-
-  public int Clamp(int min, int max, int num)
-  {
-    if (num > max)
-    {
-      return min;
+                    cells[i] = Rotate90(direction, cell);
+                    break;
+            }
+        }
     }
 
-    if (num < min)
+    public Vector3Int Rotate90(int direction, Vector3 position)
     {
-      return max;
+        float x = -1 * direction * position.y;
+        float y = direction * position.x;
+
+        return new Vector3Int((int)x, (int)y, 0);
     }
 
-    return num;
-  }
-
-
-  public bool Move(Vector2Int translation)
-  {
-    Vector3Int newPosition = position;
-    newPosition.x += translation.x;
-    newPosition.y += translation.y;
-
-    bool isValid = board.IsValidPosition(this, newPosition);
-
-    if (isValid)
+    public int Clamp(int min, int max, int num)
     {
-      this.position = newPosition;
+        if (num > max)
+        {
+            return min;
+        }
+
+        if (num < min)
+        {
+            return max;
+        }
+
+        return num;
     }
 
-    return isValid;
-  }
 
-  public void hardDrop()
-  {
-    while (Move(Vector2Int.down))
+    public bool Move(Vector2Int translation)
     {
-      continue;
+        Vector3Int newPosition = position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool isValid = board.IsValidPosition(this, newPosition);
+
+        if (isValid)
+        {
+            this.position = newPosition;
+        }
+
+        return isValid;
     }
-  }
+
+    public void hardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
+    }
 
 }
